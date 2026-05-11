7de13e3d6a68f0ba923453c1af85f3a1e6951910
Realtone <realtone98@gmail.com>
2026-05-11 11:24:32 +0900

    feat: page css

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -16,6 +16,9 @@ export default function Layout({ children }: { children: ReactNode }) {
     >
       <AppSidebar variant="inset" />
       <SidebarInset className="flex flex-col flex-1 overflow-hidden">
+        {/* sideber inset css */}
+        <div className="absolute top-0 right-0 w-[800px] h-[800px] bg-blue-500/5 rounded-full blur-[120px] pointer-events-none -translate-y-1/2 translate-x-1/3" />
+        <div className="absolute top-[40%] left-0 w-[600px] h-[600px] bg-indigo-500/5 rounded-full blur-[120px] pointer-events-none -translate-x-1/2" />
         <MainHeader />
         <div className="flex flex-1 flex-col overflow-y-auto lg:overflow-hidden min-h-0">
           <div className="@container/main flex flex-1 flex-col gap-2 min-h-0">
